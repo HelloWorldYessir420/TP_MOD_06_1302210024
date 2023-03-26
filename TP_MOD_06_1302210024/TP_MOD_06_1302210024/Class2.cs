7965dc80c96c800c7b293f1d461fd629ee03ae0a
BlyatMan221 <119160536+BlyatMan221@users.noreply.github.com>
2023-03-26 16:20:51 +0700

    implementasi design by contract

---
+++ TP_MOD_06_1302210024/TP_MOD_06_1302210024/Class2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_MOD_06_1302210024
+{
+    internal class sayaTubevideo
+    {
+        private int Id;
+        private string Title;
+        private int PlayCount;
+
+        public sayaTubevideo(string title)
+        {
+            if (title == null)
+            {
+                throw new ArgumentNullException("Judul video tidak boleh null.");
+            }
+            if (title.Length > 100)
+            {
+                throw new ArgumentException("Judul video tidak boleh lebih dari 100 karakter.");
+            }
+
+            this.Id = new Random().Next(10000, 99999);
+            this.Title = title;
+            this.PlayCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            if (count < 0 || count > 10000000)
+            {
+                throw new ArgumentException("Input penambahan play count harus di antara 0 dan 10.000.000.");
+            }
+
+            try
+            {
+                checked
+                {
+                    this.PlayCount += count;
+                }
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("ID: " + this.Id);
+            Console.WriteLine("Title: " + this.Title);
+            Console.WriteLine("Play Count: " + this.PlayCount);
+        }
+    }
+}
---
+++ TP_MOD_06_1302210024/TP_MOD_06_1302210024/Program.cs
@@ -0,0 +1,26 @@
+﻿using TP_MOD_06_1302210024;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – [Habli Zulvana Ath-Thaariq]");
+            video.PrintVideoDetails();
+
+            video.IncreasePlayCount(500);
+            video.PrintVideoDetails();
+
+            video.IncreasePlayCount(9999999);
+            video.PrintVideoDetails();
+
+            video.IncreasePlayCount(-100);
+            video.PrintVideoDetails();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+    }
+}
